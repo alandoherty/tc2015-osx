728e6132d8f9c9d455a033f56d1d6b1a5f79ee54
Jamie Hoyle <j@jamiehoyle.com>
2015-12-05 15:38:40 +0000

    added extra device stats

--- OSXWeb/js/cust.js
+++ OSXWeb/js/cust.js
@@ -1,3 +1,5 @@
+var workPercentageChart = [27, 25.3, 28];
+
 function setAppStatus(status) {
     $('.app-status').text(status);
 }
@@ -12,7 +14,14 @@ function setAppTask(task) {
 
 function setLineHeights() {
     var lineHeight = $('#clientContainer').children().children().height();
-    $('#clientContainer .client .name').css('line-height', lineHeight + "px");
+    $('#clientContainer .client').css('line-height', lineHeight + "px");
+}
+
+function setWorkPercentage() {
+    $('#clientContainer').children('.client').each(function () {
+        var devID = $(this).attr('devID') - 1;
+        $(this).children('.work').text(workPercentageChart[devID] + "%");;
+    });
 }
 
 $(document).ready(function() {
@@ -28,4 +37,6 @@ $(document).ready(function() {
     setAppStatus("online");
     setAppClients(1);
     setAppTask("testing this system");
+
+    setWorkPercentage();
 });
