56e3e8a6fcb806642be880a4cb2575cb4b9e87ce
Alan Doherty <alan.fred.doherty@googlemail.com>
2015-12-05 13:59:24 +0000

    Initial IDE directories

---
+++ OSXApp/OSXApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs">
+// Copyright (c) 2015 Alan Doherty
+// </copyright>
+//
+using System;
+
+namespace OSXApp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
